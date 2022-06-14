5047d2c681340197571a37f53f1c7bb3802220e5
Abdullah-Hasan-Sajjad <64843174+Abdullah-Hasan-Sajjad@users.noreply.github.com>
2022-06-14 18:50:21 +0600

    2nd commit

---
+++ Lab assignment 1/exp2/exp2.v
@@ -0,0 +1,17 @@
+module exp2(w,y);
+  
+input [3:0]w; 
+output reg[1:0]y; 
+  
+always @(w)  
+	casex (w)
+	
+	// priority 0>1>3>2   
+		4'bxxx1: y=0;   
+		4'bxx10: y=1;   
+		4'b1x00: y=3;   
+		4'b0100: y=2; 
+		 
+	endcase 
+endmodule
+
